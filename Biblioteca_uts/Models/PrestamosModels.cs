f3efcf2e13677a72979482f44ad3e4aaf0020293
JERH <jeelrohe@gmail.com>
2023-08-15 13:11:30 -0700

    ultimos ajusts antes de aplicar el login

--- Biblioteca_uts/Models/PrestamosModels.cs
+++ Biblioteca_uts/Models/PrestamosModels.cs
@@ -1,11 +1,17 @@
-﻿namespace Biblioteca_uts.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Biblioteca_uts.Models
 {
     public class PrestamosModels
     {
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public int IdPrestamo { get; set; }
-       public int Identificador { get; set; }
-       public DateTime Fecha_prestamo { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        public int Identificador { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        public DateTime Fecha_prestamo { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public DateTime Fecha_devolucion { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public int No_Adquisicion { get; set; }
     }
 }
--- Biblioteca_uts/Models/UsariosModels.cs
+++ Biblioteca_uts/Models/UsariosModels.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Biblioteca_uts.Models
 {
     public class UsariosModels
     {
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public int Identificador { get; set; }
-	    public string Nombres { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        public string Nombres { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string ApePa { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string ApeMa { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string Calle { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string Colonia { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string NroCasa { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string tipo { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string Contraseña { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string Usuario { get; set; }
 
     }
